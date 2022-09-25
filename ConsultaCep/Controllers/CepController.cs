8f0ee46666dcb21da44297b8067f4d28ae760e41
Joao Antonio <joao.antonio@keltecnologia.com.br>
2022-09-24 22:53:15 -0300

    zipcodelookup_commit

---
+++ ConsultaCep/Controllers/CepController.cs
@@ -0,0 +1,27 @@
+﻿using ConsultaCep.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ApiAJAX.Controllers
+{
+    public class CepController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> ConsultaCep(string id)
+        {
+            var url = $"https://viacep.com.br/ws/{id}/json";
+
+             var client = new HttpClient();
+
+            var data = await client.GetStringAsync(url);
+            
+            var retorno = JsonSerializer.Deserialize<Cep>(data);
+
+            return Json(retorno);
+        }
+    }
+}
---
+++ ConsultaCep/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using ConsultaCep.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ConsultaCep.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) => _logger = logger;
+
+        public IActionResult Index() => View();
+
+        public IActionResult Privacy() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() 
+            => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
